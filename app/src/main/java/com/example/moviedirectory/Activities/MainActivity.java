c57ffd1be60537a95483fb3f3ef0ee4420ebf5b0
naveensundar <naveennvrgup@gmail.com>
2020-01-09 12:18:32 +0530

    volley works properly

--- app/src/main/java/com/example/moviedirectory/Activities/MainActivity.java
+++ app/src/main/java/com/example/moviedirectory/Activities/MainActivity.java
@@ -2,18 +2,43 @@ package com.example.moviedirectory.Activities;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.moviedirectory.Data.MovieRecyclerViewAdapter;
+import com.example.moviedirectory.Model.Movie;
 import com.example.moviedirectory.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MovieRecyclerViewAdapter movieRecyclerViewAdapter;
+    private List<Movie> movieList;
+    private RequestQueue requestQueue;
+    private AlertDialog.Builder alertDialogBuilder;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +55,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        requestQueue= Volley.newRequestQueue(this);
+        movieList=new ArrayList<Movie>();
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        getMovies("fast and furious");
     }
 
     @Override
@@ -52,5 +85,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public List<Movie> getMovies(String searchTerm){
+        movieList.clear();
+
+        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(
+                Request.Method.GET,
+                "http://www.omdbapi.com/?&apikey=ddcb9cbc&s=" + searchTerm,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try{
+                            JSONArray movies=response.getJSONArray("Search");
+
+                            for(int i=0;i<movies.length();i++){
+                                JSONObject movieObj=movies.getJSONObject(i);
+
+                                Movie movie=new Movie();
+                                movie.setTitle(movieObj.getString("Title"));
+                                movie.setYear("Year Released: "+movieObj.getString("Year"));
+                                movie.setMovieType("Type: "+movieObj.getString("Type"));
+                                movie.setPoster(movieObj.getString("Poster"));
+                                movie.setImdbId(movieObj.getString("imdbID"));
+
+                                movieList.add(movie);
+                                Log.i("movie: ",movie.toString());
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
+
+        return movieList;
     }
 }
